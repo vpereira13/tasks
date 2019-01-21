f02c07981d5f1e5f91639a4dad4d4dc2569808f0
Victor Luiz <victor.pereira@arquivei.com.br>
2019-01-21 16:49:23 -0200

    Repository de listar todas as tarefas

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -1,12 +1,23 @@
 <?php
 
-use tarefas\core\Modules\Tasks\TaskList\Gateways\FetchGateway;
+use App\Task;
+use tasks\core\Modules\Tasks\TaskList\Gateways\FetchGateway;
 
 class TaskRepository implements FetchGateway
 {
+    private $model = Task::class;
+
+    public function __construct()
+    {
+        $this->model = new $this->model;
+    }
 
     public function findAll()
     {
+        $tasks = $this->model::all();
 
+        $tasks->save();
+
+        return $tasks;
     }
 }
