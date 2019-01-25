893f7b7080035c90ea99b05965adc777f38ca27d
Victor Luiz <victor.pereira@arquivei.com.br>
2019-01-25 16:08:16 -0200

    Criado teste unitário para TaskCollection

--- tests/Tasks/Core/Modules/Tasks/TaskList/Collections/TaskCollectionTest.php
+++ tests/Tasks/Core/Modules/Tasks/TaskList/Collections/TaskCollectionTest.php
@@ -4,6 +4,7 @@ namespace Tests\Tasks\Core\Modules\Tasks\TaskList\Collections;
 
 use PHPUnit\Framework\TestCase;
 use Task\Modules\Tasks\TaskList\Collections\TaskCollection;
+use Task\Modules\Tasks\TaskList\Entities\Task;
 
 class TaskCollectionTest extends TestCase
 {
@@ -12,5 +13,44 @@ class TaskCollectionTest extends TestCase
         $taskCollection = new TaskCollection();
 
         $this->assertInstanceOf(TaskCollection::class, $taskCollection);
+    }
+
+    public function testPushTask(): void
+    {
+        $taskCollection = new TaskCollection();
+        $task = new Task(0, (new \DateTime()), 'test', 'test');
+
+        $taskCollection->add($task);
+        $this->assertEquals(1, count($taskCollection->all()));
+    }
+
+    public function testGenericAdd(): void
+    {
+        $taskMock = $this->createMock(Task::class);
+        $taskCollection = new TaskCollection();
+
+        $taskCollection->add($taskMock);
+        $this->assertEquals(1, count($taskCollection->all()));
+    }
+
+    /**
+     * @dataProvider paramsAddProvider
+     */
+    public function testInvalidParamsAdd($task): void
+    {
+        $this->expectException(\TypeError::class);
+
+        (new TaskCollection())->add($task);
+    }
+
+    public function paramsAddProvider(): array
+    {
+        return [
+            'int' => [1],
+            'string' => ['test'],
+            'null' => [null],
+            'float' => [1.0],
+            'array' => [[]],
+        ];
     }
 }
