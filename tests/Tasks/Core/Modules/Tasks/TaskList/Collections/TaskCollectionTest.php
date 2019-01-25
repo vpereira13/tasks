d322c26be02b392e0263f3b83041fc8eb00dfb6f
Victor Luiz <victor.pereira@arquivei.com.br>
2019-01-25 14:40:41 -0200

    Adição de testes

--- tests/Tasks/Core/Modules/Tasks/TaskList/Collections/TaskCollectionTest.php
+++ tests/Tasks/Core/Modules/Tasks/TaskList/Collections/TaskCollectionTest.php
@@ -1,15 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vpereira
- * Date: 25/01/19
- * Time: 09:01
- */
 
 namespace Tests\Tasks\Core\Modules\Tasks\TaskList\Collections;
 
+use PHPUnit\Framework\TestCase;
+use Task\Modules\Tasks\TaskList\Collections\TaskCollection;
 
-class TaskCollectionTest
+class TaskCollectionTest extends TestCase
 {
+    public function testInstanceOf(): void
+    {
+        $taskCollection = new TaskCollection();
 
+        $this->assertInstanceOf(TaskCollection::class, $taskCollection);
+    }
 }
--- tests/Tasks/Core/Modules/Tasks/TaskList/Entities/TaskTest.php
+++ tests/Tasks/Core/Modules/Tasks/TaskList/Entities/TaskTest.php
@@ -9,13 +9,57 @@ class TaskTest extends TestCase
 {
     public function testInstanceOf(): void
     {
-        $task = new Task();
-
-        $task->setTaskId(1);
-        $task->setTaskDescription('asd');
-        $task->setTaskOwner('asd');
-        $task->setTaskCrerationDate(new \DateTime());
+        $task = new Task(1, (new \DateTime()), 'test', 'test');
 
         $this->assertInstanceOf(Task::class, $task);
+    }
+
+    public function testParams(): void
+    {
+        $task = new Task(1, (new \DateTime()), 'test', 'test');
+
+        $this->assertTrue(!empty($task->getTaskId()));
+        $this->assertTrue(!empty($task->getTaskCreationDate()));
+        $this->assertTrue(!empty($task->getTaskDescription()));
+        $this->assertTrue(!empty($task->getTaskOwner()));
+    }
+
+    public function testParamsEquals(): void
+    {
+        $now = new \DateTime();
+        $task = new Task(1, $now, 'test', 'test');
+
+        $this->assertEquals(1, $task->getTaskId());
+        $this->assertEquals($now, $task->getTaskCreationDate());
+        $this->assertEquals('test', $task->getTaskDescription());
+        $this->assertEquals('test', $task->getTaskOwner());
+    }
+
+    /**
+     * @dataProvider paramsProvider
+     */
+    public function testInvalidParams($id, $date, $description, $owner): void
+    {
+        $this->expectException(\TypeError::class);
+
+        new Task($id, $date, $description, $owner);
+    }
+
+    public function paramsProvider(): array
+    {
+        return  [
+            'id params' => [
+                [], 'date', 'description', 'owner'
+            ],
+            'date params' => [
+                'id', [], 'description', 'owner'
+            ],
+            'description params' => [
+                'id', 'date', [], 'owner'
+            ],
+            'owner params' => [
+                'id', 'date', 'description', []
+            ],
+        ];
     }
 }
